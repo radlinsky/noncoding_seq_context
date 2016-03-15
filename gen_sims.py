a3b6a95abbf8ba73578da13c047e4b6ba4d5834b
radlinsky <cmr273@cornell.edu>
2016-03-15 15:57:32 -0400

    fixin

--- gen_sims.py
+++ gen_sims.py
@@ -16,7 +16,7 @@ Line_s= int(sys.argv[5])
 Line_e= int(sys.argv[6])
 fasta = get_seq_context(Directory = in_DIR, Delim = delim, Col = Column_index, Start = Line_s, End = Line_e)
 
-for Line in xrange(Line_s,Line_e):
+for Line in xrange(Line_s,Line_e+1):
     seq = fasta[Line]
     new_file_path = os.path.join(in_DIR,Pop+'_1000_sim'+"_"+str(Line))
     find_simulated_variants(fastaseq = seq, pop = Pop, filesave = new_file_path, nsim = 1000)
