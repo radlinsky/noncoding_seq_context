b40916e4df829a25dcc2f41b93d55ada7fc52a1b
radlinsky <cmr273@cornell.edu>
2016-03-22 16:36:35 -0400

    documentation

--- gen_sims.py
+++ gen_sims.py
@@ -1,4 +1,33 @@
 #!/usr/bin/python
+
+### gen_sims.py
+### Caleb Matthew Radens
+### 2015_2_15
+
+###    This script runs Varun's sequence context simulator and saves results to file.
+###
+###     Arguments:
+###            in_DIR/: where is the seq_context file located? 
+###                extant, non-empty directory, that contains a single '.seq_context.BED' file
+###            delim: what are the seq_context file deliminators? (default: ,)
+###                string
+###            Column_index: which column has the fasta sequence?
+###                integer >= 0
+###            Pop: What population should the simulator use?
+###                string, "AFR", "ASN", or "EUR"
+###            Line_s: which row should fasta sequence start being pulled from to simulate on?
+###                integer, valid row index
+###            Line_e: which row to end pulling fasta sequence from?
+###                integer, valid row index
+###
+###        Depends:
+###            On Varun's find_simulated_variants module [see file path below]
+###
+###        Usage:
+###            python gen_sims.py in_DIR/ delim Column_index Pop Line_s Line_e
+
+
+#!/usr/bin/python
 import sys, os, pdb
 from subprocess import call
 home_base = '/project/voight_subrate/cradens/noncoding_seq_context/script/'
@@ -7,7 +36,7 @@ from sequence_functions import *
 varun_scripts = '/project/voight_subrate/avarun/Research/mutation_rate/scripts_for_folks'
 sys.path.append(varun_scripts)
 from find_simulated_variants import *
-#pdb.set_trace()
+
 in_DIR = str(sys.argv[1])
 delim = str(sys.argv[2])
 Column_index = int(sys.argv[3])
